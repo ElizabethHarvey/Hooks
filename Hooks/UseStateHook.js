b461496598293aeea999e0698ea81fb09da40aad
Lizzy Harvey <lizzy220188@gmail.com>
2022-08-05 11:59:37 -0400

    Added useStateHook

--- Hooks/UseStateHook.js
+++ Hooks/UseStateHook.js
@@ -1,18 +1,31 @@
-import React, { useState } from 'react';
-import { Button, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
 
 function StateHook() {
-  // Declare a new state variable, which we'll call "count"
   const [count, setCount] = useState(0);
 
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+
   return (
-    <View>
+    <View style={styles.container}>
       <Text>You clicked {count} times</Text>
-      <Button onClick={() => setCount(count + 1)}>
-        Click me
-      </Button>
+      <TouchableOpacity onPress={increment}>
+        <Text>+</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
 
 export default StateHook;
