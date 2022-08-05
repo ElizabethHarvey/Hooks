40fe546e3958bf3d2f734df163be4ea80bad6216
Lizzy Harvey <lizzy220188@gmail.com>
2022-08-05 12:39:16 -0400

    Added notes to UseStateHook

--- Hooks/UseStateHook.js
+++ Hooks/UseStateHook.js
@@ -1,18 +1,27 @@
-import { useState } from 'react';
+import { useState } from 'react'; // import useState from react / lets us keep th local state
 import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
 
 function StateHook() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0); // were declaring a new variable
+  // 0 is the initial value the button clicks start off with
+  // count holds the number of button clicks
+  // setCount lets us update the count of the initial state
 
   const increment = () => {
-    setCount((c) => c + 1);
+    setCount((c) => c + 1); // increments the count / adds 1 to the initial state or current state
   };
 
   return (
     <View style={styles.container}>
       <Text>You clicked {count} times</Text>
       <TouchableOpacity onPress={increment}>
-        <Text>+</Text>
+        <Text>
+          +
+          {
+            // when clicked we call the setCount with a new value
+            // react will re render passing th new count value to it
+          }
+        </Text>
       </TouchableOpacity>
     </View>
   );
@@ -27,5 +36,8 @@ const styles = StyleSheet.create({
   },
 });
 
-
 export default StateHook;
+
+// You will need to use useState if you need to update a value 
+// adding or subtracting
+// true or false 
